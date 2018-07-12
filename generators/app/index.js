8d53d334f2b4c725c72c99d20493f3677203e31c
Andrew Steinheiser <me@iamandrew.io>
2018-07-11 23:50:48 -0700

    add lodash functions, fix user prompt, fix help text

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,57 +1,57 @@
-var Generator = require('yeoman-generator');
-var chalk = require('chalk');
+var chalk      = require('chalk');
+var _camelCase = require('lodash.camelcase');
+var _kebabCase = require('lodash.kebabcase');
+var Generator  = require('yeoman-generator');
 
 module.exports = class extends Generator {
+
   constructor(args, opts) {
     super(args, opts);
-
+    // define options
     this.option('dumb');
     this.option('camel');
+    this.option('version');
+    // define help text
+    this.desc('\t' + chalk.yellow('React component:')
+      + '\n\t\t' + chalk.green('yo react-tools')
+      + '\n\n\t' + chalk.yellow('React Native component:')
+      + '\n\t\t' + chalk.green('yo react-tools:native'));
+    this._options.dumb.description = 'Creates a component with no state';
+    this._options.camel.description = 'CamelCases the component name instead of kebab-case';
+    this._options.version.description = 'Displays the current version number';
 
-    if(opts.camel || opts.dumb) {
-      this.log('');
-      this.log(chalk.cyan('Creating component with options:'));
-      this.log('');
-      if(opts.camel) {
-        this.log(chalk.yellow('CamelCased') + chalk.gray(' file names'));
-        this.log('');
-      }
-      if(opts.dumb) {
-        this.log(chalk.yellow('No State'));
-        this.log('');
-      }
-    }
+    this.log('');
+    this.log(chalk.cyan('Welcome to React Tools component generator!!'));
+    this.log('');
   }
 
-  // prompting() {
-  //   var self = this;
-  //   var prompts = [
-  //     {
-  //       type: 'input',
-  //       name: 'zooidName',
-  //       message: 'What is your Zooid\'s name?',
-  //       default: 'Sample Zooid'
-  //     },{
-  //       type : 'input',
-  //       name: 'author',
-  //       message: 'Who is the author?',
-  //       default: 'Octoblu Inc'
-  //     },{
-  //       type: 'input',
-  //       name: 'githubUser',
-  //       message: 'What is your github username?',
-  //       default: 'octoblu'
-  //     }
-  //   ];
-  //   return this.prompt(prompts).then(function (answers) {
-  //     var camelCased = _.camelCase(answers.zooidName);
-  //     self.zooidName = camelCased.charAt(0).toUpperCase() + camelCased.slice(1);
-  //     self.zooidNameKebab = _.kebabCase(self.zooidName);
-  //     self.author = answers.author;
-  //     self.githubUser = answers.githubUser;
-  //     self.githubUrl = "https://github.com/" + answers.githubUser;
-  //   });
-  // }
+  prompting() {
+    var self = this;
+
+    this.log(chalk.gray('Let\'s get started...'));
+    this.log('');
+
+    var prompts = [
+      {
+        type: 'input',
+        name: 'componentName',
+        message: 'What is your component\'s name?',
+        default: 'Sample Component'
+      }
+    ];
+    return this.prompt(prompts).then(function (answers) {
+
+      self.componentName = _kebabCase(answers.componentName);
+
+      if (self.options.camel) {
+        var camelCased = _camelCase(answers.componentName);
+        self.componentName = camelCased.charAt(0).toUpperCase() + camelCased.slice(1);
+      }
+      self.log('');
+      self.log(chalk.gray('Creating component: ') + chalk.green(self.componentName) + chalk.gray('...'));
+      self.log('');
+    });
+  }
 
   // writing: function() {
   //   this.composeWith('zooid:component', {
